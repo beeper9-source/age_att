e65a978ed5089700b48950e81459381703b6bb55
ku kim <beeper9@gmail.com>
2025-12-18 15:06:19 +0900

    3

---
+++ update_role_constraint.sql
@@ -0,0 +1,19 @@
+-- age_members 테이블의 role 컬럼 check constraint 업데이트
+-- Supabase SQL Editor에서 실행하세요
+
+-- 기존 constraint 삭제 (이름이 다를 수 있으므로 확인 필요)
+ALTER TABLE age_members DROP CONSTRAINT IF EXISTS age_members_role_check;
+
+-- 새로운 constraint 추가 (악장, 부악장 포함)
+ALTER TABLE age_members 
+ADD CONSTRAINT age_members_role_check 
+CHECK (role IS NULL OR role IN (
+    '단장',
+    '부단장',
+    '악장',
+    '부악장',
+    '총무',
+    '음악감독',
+    '파트장'
+));
+
